151caa34721305d29068115943936feb03549cf3
Narbe Voskanian <narbevoskanian@gmail.com>
2019-04-21 18:11:25 -0700

    run prettier code formatter

--- src/components/Hideable.js
+++ src/components/Hideable.js
@@ -1,7 +1,6 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
 // This is a component that allows you to hide/show it's children.
 // when `show` is truthy this render it's children
-export default ({show = false, children}) => (
-  show && <Fragment>{children}</Fragment>
-)
+export default ({ show = false, children }) =>
+  show && <Fragment>{children}</Fragment>;
