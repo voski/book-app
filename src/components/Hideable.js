afbf1f9791ea87012c816569c71f28e6e7032ab1
Narbe Voskanian <narbevoskanian@gmail.com>
2019-04-21 17:28:34 -0700

    destructure Fragment into a constant

--- src/components/Hideable.js
+++ src/components/Hideable.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 
 // This is a component that allows you to hide/show it's children.
 // when `show` is truthy this render it's children
 export default ({show = false, children}) => (
-  show && <React.Fragment>{children}</React.Fragment>
+  show && <Fragment>{children}</Fragment>
 )
