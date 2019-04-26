b5cc5f88ed8ff29004c8131a0bf52099d9908485
Narbe Voskanian <narbevoskanian@gmail.com>
2019-04-26 14:44:00 -0700

    Updates

---
+++ precache-manifest.54f7cda85a8e8b54e3600e787fc78709.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fd49da57e4154bfb279",
+    "url": "/book-app/static/js/runtime~main.899ff2eb.js"
+  },
+  {
+    "revision": "b756d5bf7bf7cd99e746",
+    "url": "/book-app/static/js/main.20286622.chunk.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/book-app/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "b756d5bf7bf7cd99e746",
+    "url": "/book-app/static/css/main.eab76370.chunk.css"
+  },
+  {
+    "revision": "0ffdfc8267385427eb86b85327b15658",
+    "url": "/book-app/index.html"
+  }
+];
